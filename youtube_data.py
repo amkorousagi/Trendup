ec4c0ae68d06fa6f419c2431f6ec155ab8166dea
amkorousagi <39821875+amkorousagi@users.noreply.github.com>
2020-08-09 17:01:56 +0900

    Update youtube_data.py

--- youtube_data.py
+++ youtube_data.py
@@ -103,7 +103,9 @@ def get_youtube_data_by_q(q):
         for item in res["items"]:
             get_view_count_by_id(item["id"]["videoId"])
             print("category: {}".format(q))
-            print("keyword_title: {}" + str(get_keyword(item["snippet"]["title"])).decode('cp949').encode('utf-8'))
+            print("keyword_title:")
+            for i in get_keyword(item["snippet"]["title"]):
+                print i
             print("keyword_desc: {}".format(get_keyword(item["snippet"]["description"])))
             print("viewCount: {}".format(get_view_count_by_id(item["id"]["videoId"])))
             print("publish time: {}".format(item["snippet"]["publishTime"]))
