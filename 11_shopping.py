58c2902b7734679171aa66e0e74d9e69de0e3369
JEONYUNTAE <67727745+JEONYUNTAE@users.noreply.github.com>
2020-08-09 16:07:44 +0900

    Update 11_shopping.py

--- 11_shopping.py
+++ 11_shopping.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import requests
 import pymysql.cursors
 
@@ -15,7 +17,7 @@ def swithch_gender(x):
         1:'여성'
     }.get(x,-1)
 
-def counter(input_list):
+def counter(keyword):
     word_count = {}
     for word in keyword:
         if word in word_count:
@@ -36,69 +38,70 @@ conn = pymysql.connect(
     )
 curs=conn.cursor()
 
-for i in gender:
+def _11_shopping():
+    for i in gender:
 
-    url = "http://www.11st.co.kr/browsing/BestSeller.tmall?method=getBestSellerMain&cornerNo=2&dispCtgrNo="+switch_site(i)
+        url = "http://www.11st.co.kr/browsing/BestSeller.tmall?method=getBestSellerMain&cornerNo=2&dispCtgrNo="+switch_site(i)
 
-    custom_header = {
-        "user-agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36"
-    }
-    req = requests.get(url, headers=custom_header)
+        custom_header = {
+            "user-agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36"
+        }
+        req = requests.get(url, headers=custom_header)
 
-    html = BeautifulSoup(req.text, "html.parser")
+        html = BeautifulSoup(req.text, "html.parser")
 
-    items = html.select("p")
+        items = html.select("p")
 
-    allword = []
-    keyword = []
+        allword = []
+        keyword = []
 
-    for item in items:
-        allword.append(item.text.strip())
+        for item in items:
+            allword.append(item.text.strip())
 
-    first_one = 0
-    for j in range(0, 10):
-        allword.remove(allword[first_one])
+        first_one = 0
+        for j in range(0, 10):
+            allword.remove(allword[first_one])
 
-    split_list = []
+        split_list = []
 
-    for j in allword:
-        r_j = j.replace('[', ' ').replace(']', ' ').replace('/', ' ').replace('(', ' ').replace(')', ' ')
-        split_list = r_j.split(" ")
-        for k in split_list:
-            keyword.append(k)
+        for j in allword:
+            r_j = j.replace('[', ' ').replace(']', ' ').replace('/', ' ').replace('(', ' ').replace(')', ' ')
+            split_list = r_j.split(" ")
+            for k in split_list:
+                keyword.append(k)
 
-    keyword = ' '.join(keyword).split()
+        keyword = ' '.join(keyword).split()
 
-    word_count = counter(keyword)
-    word_count = sorted(word_count.items(), key=lambda x: x[1], reverse=True)
+        word_count = counter(keyword)
+        word_count = sorted(word_count.items(), key=lambda x: x[1], reverse=True)
 
-    #keyword 삭제
-    banlist = ['남성','남자','여성','여자']
+        #keyword 삭제
+        banlist = ['남성','남자','여성','여자']
 
-    x=0
-    for j in word_count:
-        if j[0] in banlist:
-            del word_count[x]
-            x=x+1
-        else:
-            x=x+1
+        x=0
+        for j in word_count:
+            if j[0] in banlist:
+                del word_count[x]
+                x=x+1
+            else:
+                x=x+1
 
-    keyword2=[]
-    #상위 30개만 추출
-    for j in range(0,30):
-        keyword2.append(word_count[j])
+        keyword2=[]
+        #상위 20개만 추출
+        for j in range(0,20):
+            keyword2.append(word_count[j])
 
-    print(keyword2)
 
-    k=1
-    for j in keyword2:
-        gender_=swithch_gender(i)
-        values1=(str(k),j[0],str(swithch_gender(i)),str(j[1]))
-        query1="insert into 11_shopping (rank,keyword,date_,gender,score) values(%s,%s,cast(now() as char),%s,%s)"
+        k=1
+        for j in keyword2:
+            gender_=swithch_gender(i)
+            values1=(str(k),j[0],str(swithch_gender(i)),str(j[1]))
+            query1="insert into 11_shopping (rank,keyword,date_,gender,score) values(%s,%s,cast(now() as char),%s,%s)"
 
-        curs.execute(query1,values1)
+            curs.execute(query1,values1)
 
-        k=k+1
+            k=k+1
+
 
 conn.commit()
 conn.close()
