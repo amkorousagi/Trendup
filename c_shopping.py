c5b271caf9fbaa1305c864b5a294f55fcec828df
JEONYUNTAE <67727745+JEONYUNTAE@users.noreply.github.com>
2020-08-09 15:51:46 +0900

    Update c_shopping.py

--- c_shopping.py
+++ c_shopping.py
@@ -36,65 +36,62 @@ conn = pymysql.connect(
     )
 curs=conn.cursor()
 
-for i in gender:
+def c_shopping():
+    for i in gender:
 
-    url ="https://www.coupang.com/np/categories/"+switch_site(i)
+        url ="https://www.coupang.com/np/categories/"+switch_site(i)
 
-    custom_header = {
-        "user-agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36"
-    }
-    req = requests.get(url, headers=custom_header)
+        custom_header = {
+            "user-agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36"
+        }
+        req = requests.get(url, headers=custom_header)
 
-    html = BeautifulSoup(req.text, "html.parser")
+        html = BeautifulSoup(req.text, "html.parser")
 
-    items = html.select("div.name")
+        items = html.select("div.name")
 
-    allword = []
-    keyword = []
+        allword = []
+        keyword = []
 
-    for item in items:
-        allword.append(item.text.strip())
+        for item in items:
+            allword.append(item.text.strip())
 
-    split_list = []
+        split_list = []
 
-    for j in allword:
-        r_j = j.replace('[', ' ').replace(']', ' ').replace('/', ' ').replace('(', ' ').replace(')', ' ')
-        split_list = r_j.split(" ")
-        for k in split_list:
-            keyword.append(k)
+        for j in allword:
+            r_j = j.replace('[', ' ').replace(']', ' ').replace('/', ' ').replace('(', ' ').replace(')', ' ')
+            split_list = r_j.split(" ")
+            for k in split_list:
+                keyword.append(k)
 
-    keyword = ' '.join(keyword).split()
+        keyword = ' '.join(keyword).split()
 
-    word_count = counter(keyword)
-    word_count = sorted(word_count.items(), key=lambda x: x[1], reverse=True)
+        word_count = counter(keyword)
+        word_count = sorted(word_count.items(), key=lambda x: x[1], reverse=True)
 
-    #keyword 삭제
-    banlist = ['남성','남자','여성','여자']
+        #keyword 삭제
+        banlist = ['남성','남자','여성','여자']
 
-    x=0
-    for j in word_count:
-        if j[0] in banlist:
-            del word_count[x]
-            x=x+1
-        else:
-            x=x+1
+        x=0
+        for j in word_count:
+            if j[0] in banlist:
+                del word_count[x]
+                x=x+1
+            else:
+                x=x+1
 
-    keyword2=[]
-    #상위 30개만 추출
-    for j in range(0,30):
-        keyword2.append(word_count[j])
+        keyword2=[]
+        #상위 30개만 추출
+        for j in range(0,30):
+            keyword2.append(word_count[j])
 
-    print(keyword2)
-
-    k=1
-    for j in keyword2:
-        gender_=swithch_gender(i)
-        values1=(str(k),j[0],str(swithch_gender(i)),str(j[1]))
-        query1="insert into c_shopping (rank,keyword,date_,gender,score) values(%s,%s,cast(now() as char),%s,%s)"
-
-        curs.execute(query1,values1)
-
-        k=k+1
+        k=1
+        for j in keyword2:
+            gender_=swithch_gender(i)
+            values1=(str(k),j[0],str(swithch_gender(i)),str(j[1]))
+            query1="insert into c_shopping (rank,keyword,date_,gender,score) values(%s,%s,cast(now() as char),%s,%s)"
+            curs.execute(query1,values1)
+            k=k+1
 
 conn.commit()
 conn.close()
